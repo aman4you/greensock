501e78f1fa53ce26f4fc3858a1a2b79dc9222aaf
innoraft <aman.goel@innoraft.com>
2016-07-26 18:06:01 +0530

    Add internet block

--- sites/all/themes/bootstrap_sub/templates/page--node--5.tpl.php
+++ sites/all/themes/bootstrap_sub/templates/page--node--5.tpl.php
@@ -1,3 +1,29 @@
 <div class="cloud-block">
 	<div class="cloud-image"></div>
 </div>
+
+<div class="interset-area-main">
+	<div class="interset-area">
+		<div class="left-container">
+			<ul>
+				<li><span class="icons icon-iot"></span></li>
+				<li><span class="icons icon-ai"></span></li>
+				<li><span class="icons icon-auto"></span></li>
+				<li><span class="icons icon-m2m"></span></li>
+				<li><span class="icons icon-science"></span></li>
+			</ul>
+		</div>
+		<div class="main-image">
+			<img src="/sites/all/themes/bootstrap_sub/img/interset-area.png">
+		</div>
+		<div class="right-container">
+			<ul>
+				<li><span class="icons icon-healthcare"></span></li>
+				<li><span class="icons icon-financial"></span></li>
+				<li><span class="icons icon-energy"></span></li>
+				<li><span class="icons icon-media"></span></li>
+				<li><span class="icons icon-social"></span></li>
+			</ul>
+		</div>
+	</div>
+</div>
