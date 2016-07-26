55677c3957b8b36ae6bec20db5bef7d03d1225c8
innoraft <aman.goel@innoraft.com>
2016-07-27 01:09:46 +0530

    Apply scollMagic to internet block

--- sites/all/themes/bootstrap_sub/js/css3-animate2.js
+++ sites/all/themes/bootstrap_sub/js/css3-animate2.js
@@ -1,7 +1,28 @@
 (function($) {
 
-	$(document).ready(function() {
+	// $(window).scroll(function() {
+	// 	$('.internet-area-main .slide-right').each(function() {
+	// 		if ($(window).scrollTop() == $(this).offset().top - 400) {
+	// 			console.log($('.internet-area-main .row').attr('class'));
+	// 		}
+	// 	})
+	// });
 
+	$(document).ready(function() {
+		var controller = new ScrollMagic.Controller();
+
+		$('.internet-area-main .slide-right').each(function() {
+			var ourScene = new ScrollMagic.Scene({
+				triggerElement: this,
+				triggerHook: 0.75
+			})
+			.on('start', callback)
+			.addTo(controller);
+		});
+
+		function callback(event) {
+			var triggerItem = this.triggerElement().className;
+		}
 	});
 
 })(jQuery);
--- sites/all/themes/bootstrap_sub/templates/page--node--5.tpl.php
+++ sites/all/themes/bootstrap_sub/templates/page--node--5.tpl.php
@@ -2,15 +2,15 @@
 	<div class="cloud-image"></div>
 </div>
 
-<div class="interset-area-main">
-	<div class="interset-area">
+<div class="internet-area-main">
+	<div class="internet-area">
 		<div class="left-container">
 			<ul>
-				<li><span class="icons icon-iot"></span></li>
-				<li><span class="icons icon-ai"></span></li>
-				<li><span class="icons icon-auto"></span></li>
-				<li><span class="icons icon-m2m"></span></li>
-				<li><span class="icons icon-science"></span></li>
+				<li id="abc" class="row-1 slide-right"><span class="icons icon-iot"></span></li>
+				<li class="row-2 slide-right"class=""><span class="icons icon-ai"></span></li>
+				<li class="row-3 slide-right"><span class="icons icon-auto"></span></li>
+				<li class="row-4 slide-right"><span class="icons icon-m2m"></span></li>
+				<li class="row-5 slide-right"><span class="icons icon-science"></span></li>
 			</ul>
 		</div>
 		<div class="main-image">
@@ -18,11 +18,11 @@
 		</div>
 		<div class="right-container">
 			<ul>
-				<li><span class="icons icon-healthcare"></span></li>
-				<li><span class="icons icon-financial"></span></li>
-				<li><span class="icons icon-energy"></span></li>
-				<li><span class="icons icon-media"></span></li>
-				<li><span class="icons icon-social"></span></li>
+				<li class="row-1 slide-left"><span class="icons icon-healthcare"></span></li>
+				<li class="row-2 slide-left"><span class="icons icon-financial"></span></li>
+				<li class="row-3 slide-left"><span class="icons icon-energy"></span></li>
+				<li class="row-4 slide-left"><span class="icons icon-media"></span></li>
+				<li class="row-5 slide-left"><span class="icons icon-social"></span></li>
 			</ul>
 		</div>
 	</div>
